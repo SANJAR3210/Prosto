f4e730e443c238c097f0e46aea7475609977a77d
Sanjar <ahmedovsanjar935@gmail.com>
2023-12-01 22:37:15 +0700

    Программа использует класс Ball для представления шарика у каждого шарика есть координаты (x, y), радиус, скорость и угол движения метод update обновляет координаты шарика в соответствии с его скоростью и углом движения метод draw отрисовывает шарик на экране с помощью функции pygame.draw.circle. главный цикл программы обрабатывает события мыши и обновляет и отрисовывает каждый шарик на экране время, прошедшее с предыдущего кадра, вычисляется с помощью функции clock.tick и используется для плавного движения шариков после запуска программы можно щелкнуть левой кнопкой мыши в любом месте окна, чтобы создать новый шарик шарики будут двигаться влево-вверх и отскакивать от границ окна скорость шариков рассчитывается с использованием следующей формулы:
    dx = self.speed * math.cos(self.angle) * dt
    dy = -self.speed * math.sin(self.angle) * dt
    
    если что angle = угол на английском
    для подсчёта используются радианы так как в вычислениях в библиотекой math их и спользуют(попробуй впихать в 22 и 23 строки синус и косинус угла 45 градусов :) https://www.test-uz.ru/img_p/brad.webp)

--- third.py
+++ third.py
@@ -1,28 +1,24 @@
 import pygame
 import math
 
-# Инициализация Pygame
 pygame.init()
-
-# Создание окна
 size = width, height = 800, 800
-screen = pygame.display.set_mode((size))
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Шарики")
 
-# Класс для представления шарика
-class Ball():
+
+class Ball:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.speed = 100
         self.angle = math.radians(45)
 
-    def update(self, dt):
-        # Вычисление новых координат шарика
-        dx = self.speed * math.cos(self.angle) * dt
-        dy = -self.speed * math.sin(self.angle) * dt
-        self.x -= dx
-        self.y += dy
+    def new_coord(self, vremya):
+        vx = self.speed * math.cos(self.angle) * vremya
+        vy = -self.speed * math.sin(self.angle) * vremya
+        self.x -= vx
+        self.y += vy
 
         # Проверка столкновения с границами окна
         if self.x - 10 < 0 or self.x + 10 > width:
@@ -33,12 +29,13 @@ class Ball():
     def draw(self):
         pygame.draw.circle(screen, (255, 255, 255), (int(self.x), int(self.y)), 10)
 
+
 # Список шариков
 balls = []
 running = True
 clock = pygame.time.Clock()
 while running:
-    dt = clock.tick(60) / 1000  # Вычисление времени, прошедшего с предыдущего кадра
+    vremya = clock.tick(60) / 1000  # Вычисление времени, прошедшего с предыдущего кадра
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -48,7 +45,7 @@ while running:
     screen.fill((0, 0, 0))
     # Обновление и рисование каждого шарика
     for ball in balls:
-        ball.update(dt)
+        ball.new_coord(vremya)
         ball.draw()
     pygame.display.flip()
 pygame.quit()
